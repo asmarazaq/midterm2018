1445780bdcbf789981b591a67b8e604b70fd96cc
asmarazaq <asma.umar972@gmail.com>
2018-12-23 16:41:45 -0500

    math.problem: Fibonacci,Factorial, Find Missing number, Lowest Difference, Pyramid, part of Core java Question added

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,12 +1,18 @@
 package string.problems;
 
-/**
- * Created by mrahman on 04/22/17.
- */
+import java.util.Arrays;
+
 public class Anagram {
 
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        String b= "ARMY";
+        String e= "MARY";
+
+        //System.out.println(isAnagram(b,e));
     }
+
+
 }
